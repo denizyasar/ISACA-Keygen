694de4bda7085ad23890cb1c52133d3fc229f686
Deniz Yaşar <dyasar@outlook.com>
2022-02-01 22:52:29 +0300

    Add files via upload

---
+++ frmMain.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace ISACA_Keygen
+{
+  partial class FrmMain
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.txtInstallCode = new System.Windows.Forms.TextBox();
+            this.GenSerial = new System.Windows.Forms.Button();
+            this.txtHardwareCode = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cboxExam = new System.Windows.Forms.ComboBox();
+            this.GenActivation = new System.Windows.Forms.Button();
+            this.txtActivationCode = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtInstallCode
+            // 
+            this.txtInstallCode.Location = new System.Drawing.Point(107, 57);
+            this.txtInstallCode.Name = "txtInstallCode";
+            this.txtInstallCode.ReadOnly = true;
+            this.txtInstallCode.Size = new System.Drawing.Size(171, 20);
+            this.txtInstallCode.TabIndex = 0;
+            // 
+            // GenSerial
+            // 
+            this.GenSerial.Location = new System.Drawing.Point(107, 83);
+            this.GenSerial.Name = "GenSerial";
+            this.GenSerial.Size = new System.Drawing.Size(171, 25);
+            this.GenSerial.TabIndex = 1;
+            this.GenSerial.Text = "Generate Install Code";
+            this.GenSerial.UseVisualStyleBackColor = true;
+            this.GenSerial.Click += new System.EventHandler(this.GenSerial_Click);
+            // 
+            // txtHardwareCode
+            // 
+            this.txtHardwareCode.Location = new System.Drawing.Point(107, 114);
+            this.txtHardwareCode.Name = "txtHardwareCode";
+            this.txtHardwareCode.Size = new System.Drawing.Size(171, 20);
+            this.txtHardwareCode.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Install Code :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Hardware Code :";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.cboxExam);
+            this.groupBox1.Controls.Add(this.GenActivation);
+            this.groupBox1.Controls.Add(this.txtActivationCode);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.GenSerial);
+            this.groupBox1.Controls.Add(this.txtInstallCode);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtHardwareCode);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(300, 236);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Coded by sea1731";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(3, 205);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(295, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Choose \"Manually via the Web\" Activation Method";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Choose Exam :";
+            // 
+            // cboxExam
+            // 
+            this.cboxExam.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboxExam.Items.AddRange(new object[] {
+            "CISA 2013",
+            "CISM 2013",
+            "CISA 2014",
+            "CISM 2014",
+            "CISA 2015",
+            "CISM 2015",
+            "CISA 2016",
+            "CISM 2016"});
+            this.cboxExam.Location = new System.Drawing.Point(107, 30);
+            this.cboxExam.Name = "cboxExam";
+            this.cboxExam.Size = new System.Drawing.Size(171, 21);
+            this.cboxExam.TabIndex = 9;
+            // 
+            // GenActivation
+            // 
+            this.GenActivation.Location = new System.Drawing.Point(107, 166);
+            this.GenActivation.Name = "GenActivation";
+            this.GenActivation.Size = new System.Drawing.Size(171, 25);
+            this.GenActivation.TabIndex = 6;
+            this.GenActivation.Text = "Generate Activation Code";
+            this.GenActivation.UseVisualStyleBackColor = true;
+            this.GenActivation.Click += new System.EventHandler(this.GenActivation_Click);
+            // 
+            // txtActivationCode
+            // 
+            this.txtActivationCode.Location = new System.Drawing.Point(107, 140);
+            this.txtActivationCode.Name = "txtActivationCode";
+            this.txtActivationCode.Size = new System.Drawing.Size(171, 20);
+            this.txtActivationCode.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Activation Code :";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 236);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.Text = "ISACA 2016 - Keygen";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TextBox txtInstallCode;
+    private System.Windows.Forms.Button GenSerial;
+    private System.Windows.Forms.TextBox txtHardwareCode;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.GroupBox groupBox1;
+    private System.Windows.Forms.Button GenActivation;
+    private System.Windows.Forms.TextBox txtActivationCode;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.ComboBox cboxExam;
+    private System.Windows.Forms.Label label5;
+  }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace ISACA_Keygen
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+            cboxExam.SelectedIndex = 0;
+        }
+
+        private void GenActivation_Click(object sender, EventArgs e)
+        {
+            if (txtHardwareCode.Text != string.Empty)
+            {
+                try
+                {
+                    var str = Regex.Replace(txtInstallCode.Text, @"\s+", "");
+                    txtActivationCode.Text = GenerateActivationCode(str.Substring(str.Length - 3)).ToUpperInvariant();
+                }
+                catch (Exception ex)
+                {
+                    txtActivationCode.Text = ex.Message;
+                }
+            }
+        }
+
+        private string GenerateActivationCode(string partsMask)
+        {
+            var str =
+                StringToDigits(Encrypt(Encrypt(Encrypt("Activate", "MLS Inc."), txtInstallCode.Text),
+                                       txtHardwareCode.Text))
+                .Substring(0, 14);
+            var builder = new StringBuilder();
+            builder.Append(partsMask);
+            builder.Append(str);
+            var checkSum = GetCheckSum(builder.ToString(), 0x3e7);
+            var builder2 = new StringBuilder();
+            builder2.Append(checkSum);
+            builder2.Append(str.Substring(0, 2));
+            builder2.Append("-");
+            builder2.Append(str.Substring(2, 5));
+            builder2.Append("-");
+            builder2.Append(str.Substring(7, 5));
+            builder2.Append("-");
+            builder2.Append(str.Substring(12));
+            builder2.Append(partsMask);
+            return builder2.ToString();
+        }
+
+        private void GenSerial_Click(object sender, EventArgs e)
+        {
+            string str3, checkSum, key = string.Empty;
+
+            switch (cboxExam.SelectedIndex)
+            {
+                case 0:
+                    key = "ICISA-2013.0-";
+                    break;
+                case 1:
+                    key = "ICISM-2013.0-";
+                    break;
+                case 2:
+                    key = "ICISA-2014.0-";
+                    break;
+                case 3:
+                    key = "ICISM-2014.0-";
+                    break;
+                case 4:
+                    key = "ICISA-2015.0-";
+                    break;
+                case 5:
+                    key = "ICISM-2015.0-";
+                    break;
+                case 6:
+                    key = "ICISA-2016.0-";
+                    break;
+                case 7:
+                    key = "ICISM-2016.0-";
+                    break;
+                default:
+                    break;
+            }
+
+            do
+            {
+                txtInstallCode.Text = key;
+
+                var s = string.Empty;
+                var generated = new HashSet<int>();
+                var r = new Random();
+                for (var i = 1; i < 3; i++)
+                {
+                    int a;
+                    do
+                    {
+                        a = r.Next(10000, 99999);
+                    } while (generated.Contains(a));
+                    generated.Add(a);
+
+                    s = s + a + "-";
+                }
+                txtInstallCode.Text = $"{txtInstallCode.Text}{s}987";
+                var code = (Regex.Replace(txtInstallCode.Text, @"\s+", "")).ToUpperInvariant();
+
+                var parts = code.Split('-');
+
+                var builder = new StringBuilder();
+                var str = parts[3].Substring(2) + parts[4].Substring(0, 2);
+                builder.Append(parts[4].Substring(2));
+                builder.Append(str);
+                builder.Append(parts[2]);
+                builder.Append(parts[1]);
+                builder.Append(parts[0]);
+                checkSum = GetCheckSum(builder.ToString(), 0x63);
+                str3 = parts[3].Substring(0, 2);
+            } while (str3 != checkSum);
+        }
+
+        private static int GetCharacterValue(string character)
+        {
+            if (character.Length != 1)
+                throw new ArgumentException("Character string is too long.");
+            return character.ToCharArray()[0];
+        }
+
+        private static PasswordDeriveBytes GetPasswordDerivedBytes(string password)
+        {
+            return new PasswordDeriveBytes(password,
+                                           new byte[]
+                { 0x49, 0x76, 0x61, 110, 0x20, 0x4d, 0x65, 100, 0x76, 0x65, 100, 0x65, 0x76 });
+        }
+
+        public static string Encrypt(string clearText, string password)
+        {
+            var clearData = Encoding.Unicode.GetBytes(clearText);
+            var passwordDerivedBytes = GetPasswordDerivedBytes(password);
+            return
+                Convert.ToBase64String(Encrypt(clearData,
+                                               passwordDerivedBytes.GetBytes(0x20),
+                                               passwordDerivedBytes.GetBytes(0x10)));
+        }
+
+        public static byte[] Encrypt(byte[] clearData, byte[] key, byte[] IV)
+        {
+            var stream = new MemoryStream();
+            var rijndael = Rijndael.Create();
+            rijndael.Key = key;
+            rijndael.IV = IV;
+            var stream2 = new CryptoStream(stream, rijndael.CreateEncryptor(), CryptoStreamMode.Write);
+            stream2.Write(clearData, 0, clearData.Length);
+            stream2.Close();
+            return stream.ToArray();
+        }
+
+
+        public static string GetCheckSum(string chars, int maxValue)
+        {
+            chars = chars.ToUpperInvariant();
+            var num = 0;
+            for (var i = 0; i < chars.Length; i++)
+            {
+                num += GetCharacterValue(chars.Substring(i, 1));
+                while (num > maxValue)
+                    num = (num / 10) + (num % 10);
+            }
+            var length = maxValue.ToString().Length;
+            var str = num.ToString();
+            if (str.Length < length)
+            {
+                str = "000000000" + str;
+                str = str.Substring(str.Length - length);
+            }
+            return str;
+        }
+
+        public static byte[] StringToBytes(string data) { return new ASCIIEncoding().GetBytes(data); }
+
+
+        public static string StringToDigits(string data)
+        {
+            var buffer = StringToBytes(data);
+            var builder = new StringBuilder();
+            foreach (var num in buffer)
+            {
+                var str = "0000" + num;
+                str = str.Substring(str.Length - 3);
+                builder.Append(str.Substring(2, 1));
+                builder.Append(str.Substring(0, 1));
+                builder.Append(str.Substring(1, 1));
+            }
+            return builder.ToString();
+        }
+    }
+}
